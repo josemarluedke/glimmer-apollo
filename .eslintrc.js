7784baf5d9750d987f2857ef8be42b2e7c300eba
Josemar Luedke <josemarluedke@gmail.com>
2021-03-17 17:24:28 -0700

    Initial Project Setup

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+const { join } = require('path');
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    project: join(__dirname, './tsconfig.eslint.json')
+  },
+  plugins: [],
+  extends: ['@underline/eslint-config-ember-typescript'],
+  rules: {
+    '@typescript-eslint/no-empty-interface': 'off',
+    'ember/no-empty-glimmer-component-classes': 'off'
+  },
+  overrides: [
+    {
+      files: ['packages/**/tests/**/*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off'
+      }
+    },
+
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.prettierrc.js',
+        '.template-lintrc.js',
+        'testem.js',
+        'config/**/*.js',
+        'packages/*/.ember-cli.js',
+        'packages/*/ember-cli-build.js',
+        'packages/*/index.js',
+        'packages/*/testem.js',
+        'packages/*/blueprints/*/index.js',
+        'packages/*/config/**/*.js',
+        'packages/*/tests/dummy/config/**/*.js',
+        'packages/**/tailwind.config.js',
+        'packages/**/tailwind/*.js',
+        'packages/tailwindcss-plugin-helpers/**/*.js',
+        'site/ember-cli-build.js',
+        'site/testem.js',
+        'site/config/**/*.js',
+        'site/tests/dummy/config/**/*.js',
+        'site/**/tailwind.config.js',
+        'site/.docfy-config.js',
+        'site/lib/docfy-theme/*.js'
+      ],
+      extends: ['@underline/eslint-config-node'],
+      rules: {}
+    }
+  ]
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# glimmer-apollo
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
