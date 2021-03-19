75c50c237f8cbba6d0b5f2fc57658397fb1770ae
Josemar Luedke <josemarluedke@gmail.com>
2021-03-19 11:18:55 -0700

    Setup msw for GraphQL mocking

---
+++ packages/glimmer-apollo/tests/dummy/app/mocks/handlers.ts
@@ -0,0 +1,48 @@
+import { graphql } from 'msw';
+
+export const handlers = [
+  // Capture a "Login" mutation
+  graphql.mutation('Login', (req, res, ctx) => {
+    const { username } = req.variables;
+
+    if (username === 'non-existing') {
+      return res(
+        ctx.errors([
+          {
+            message: 'User not found',
+            extensions: {
+              id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda'
+            }
+          }
+        ])
+      );
+    }
+
+    return res(
+      ctx.data({
+        user: {
+          __typename: 'User',
+
+          id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+          firstName: 'John',
+          lastName: 'Maverick'
+        }
+      })
+    );
+  }),
+
+  // Handles a "GetUserInfo" query
+  graphql.query('GetUserInfo', (_, res, ctx) => {
+    return res(
+      ctx.data({
+        user: {
+          __typename: 'User',
+
+          id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+          firstName: 'John',
+          lastName: 'Maverick'
+        }
+      })
+    );
+  })
+];
--- packages/glimmer-apollo/tests/dummy/app/routes/application.ts
+++ packages/glimmer-apollo/tests/dummy/app/routes/application.ts
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
+import config from 'dummy/config/environment';
+import { worker } from 'dummy/mocks/browser';
 
 export default class ApplicationRoute extends Route {
-  model() {}
+  async beforeModel(): Promise<void> {
+    if (config.environment === 'development') {
+      await worker.start();
+    }
+  }
 }
