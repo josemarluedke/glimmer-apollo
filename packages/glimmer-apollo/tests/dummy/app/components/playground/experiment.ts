5ff591d43f08be44f48a963a940015a0233288f1
Josemar Luedke <josemarluedke@gmail.com>
2021-03-19 21:12:40 -0700

    Add a few experiments in playground for development

---
+++ packages/glimmer-apollo/tests/dummy/app/components/playground/experiment.ts
@@ -0,0 +1,37 @@
+import Component from '@glimmer/component';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from 'glimmer-apollo';
+
+const USER_INFO = gql`
+  query GetUserInfo {
+    user {
+      username
+      firstName
+    }
+  }
+`;
+
+const LOGIN = gql`
+  mutation Login($username: String!) {
+    login(username: $username) {
+      id
+      firstName
+    }
+  }
+`;
+
+export default class PlaygroundExperiment extends Component {
+  userInfo = useQuery(this, () => [USER_INFO]);
+  login = useMutation(this, () => [
+    LOGIN,
+    {
+      variables: {
+        // username: 'non-existing'
+      }
+    }
+  ]);
+
+  bla = (): void => {
+    this.login.mutate();
+  };
+}
