79daae97c7f52a7c4afcd05ddc826364eef47ec0
Josemar Luedke <josemarluedke@gmail.com>
2021-04-03 10:07:59 -0700

    Add apollo client to glimmerx app

---
+++ packages/test-glimmerx/src/apollo.ts
@@ -0,0 +1,17 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client/core';
+import { setClient } from 'glimmer-apollo';
+
+export default function createClient(): void {
+  setClient(
+    new ApolloClient({
+      cache: new InMemoryCache(),
+      link: createHttpLink({
+        uri: '/graphql'
+      })
+    }) as never
+  );
+}
