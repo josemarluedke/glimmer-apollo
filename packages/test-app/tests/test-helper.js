f08f01ffe9bd2aabcb2a11e952cb8b9f78e14cd5
Josemar Luedke <josemarluedke@gmail.com>
2021-05-23 20:17:29 -0700

    Import glimmer-apollo tests into test-app

--- packages/test-app/tests/test-helper.js
+++ packages/test-app/tests/test-helper.js
@@ -4,9 +4,13 @@ import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import { worker } from 'test-app/mocks/browser';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
 
-start();
+(async function () {
+  await worker.start();
+  start();
+})();
