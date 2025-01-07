114ae10086f082e75809c09182b2cb5045892447
Josemar Luedke <josemarluedke@gmail.com>
2025-01-07 14:15:25 -0800

    chore: remove example glimmerx app

--- examples/glimmerx/src/mock/factories.ts
+++
@@ -1,9 +0,0 @@
-import { Factory } from 'miragejs';
-
-const note = Factory.extend({
-  title: (i: number) => `Note ${i}`,
-  description: 'Description',
-  isArchived: (i: number) => !(i % 2)
-});
-
-export default { note };
