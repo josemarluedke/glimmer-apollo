aeb8274d07dde181d4285e6e011cd0e3f8c88e10
Josemar Luedke <josemarluedke@gmail.com>
2025-01-09 08:33:57 -0800

    move addon out of packages folder

--- packages/glimmer-apollo/types/@glimmer/tracking/primitives/cache.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@glimmer/tracking/primitives/cache' {
-  export type Cache<T = unknown> = {}; // eslint-disable-line
-
-  export function createCache<T>(fn: () => T): Cache<T>;
-
-  export function getValue<T>(cache: Cache<T>): T;
-
-  export function isConst(cache: Cache): boolean;
-}
