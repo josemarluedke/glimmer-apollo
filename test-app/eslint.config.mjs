8c285470a6a705b6e7e40e77eed4e28a77e9d5e1
Josemar Luedke <josemarluedke@gmail.com>
2025-01-17 16:48:09 -0800

    chore: ignore vite.config.mjs.timestamp*

--- test-app/eslint.config.mjs
+++ test-app/eslint.config.mjs
@@ -146,7 +146,7 @@ export default [
    * Settings
    */
   {
-    ignores: ['dist/', 'node_modules/', 'coverage/', '!**/.*'],
+    ignores: ['dist/', 'node_modules/', 'coverage/', '!**/.*', 'vite.config.mjs.timestamp*'],
     linterOptions: {
       reportUnusedDisableDirectives: 'error',
     },
